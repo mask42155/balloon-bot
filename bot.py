1105dae26820076800c623ed9641f2f1ff319f93
mask42155 <goodmask777@gmail.com>
2025-10-22 04:09:14 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,17 @@
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет! 🎈 Бот работает!")
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
+
